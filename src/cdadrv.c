c9f9f693d4a45c8ea23e86c2b6e16a1c2999c66a
Evan Alekseyev <54689704+ealekseyev@users.noreply.github.com>
2026-08-03 15:21:32 -0700

    Update module version to 0.6.0.2 (#26)

--- src/cdadrv.c
+++ src/cdadrv.c
@@ -22,7 +22,7 @@
 MODULE_AUTHOR("DeGirum Corp., Egor Pomozov");
 MODULE_DESCRIPTION("CDA linux driver to access pci devices");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("0.6.0.1");
+MODULE_VERSION("0.6.0.2");
 // The version has to be in the format n.n.n.n, where each n is a single digit
 
 #if KERNEL_VERSION(4, 9, 0) > LINUX_VERSION_CODE
